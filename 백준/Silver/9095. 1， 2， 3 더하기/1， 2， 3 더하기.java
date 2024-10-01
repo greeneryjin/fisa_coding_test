a9f11cfd3dab402e5106100d7e04c1ec5467dbf7
KimEoJin <87289562+greeneryjin@users.noreply.github.com>
2024-10-01 15:30:49 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14056 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,33 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
-    static int x = 0;
-    static int dp[] = new int [11];
-    
+
     public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(br.readLine());
-        x = Integer.parseInt(st.nextToken());
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[11];
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 4;
-        
-        for(int i = 0; i < x; i++) {
-            st = new StringTokenizer(br.readLine());
-            int data = Integer.parseInt(st.nextToken());
-        	dps(data);
+
+        for (int i = 4; i <= 10; i++) {
+            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            int m = Integer.parseInt(br.readLine());
+            System.out.println(dp[m]);
         }
     }
-
-	private static void dps(int data) {
-        for(int i = 4; i <= 10; i++) {
-        	dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
-        }
-        
-        System.out.println(dp[data]);
-	}
 }
