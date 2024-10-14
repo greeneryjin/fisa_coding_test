dfdb6a6c3e6afd401d82cd278c3d490d5374f1f2
KimEoJin <87289562+greeneryjin@users.noreply.github.com>
2024-10-14 16:27:49 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14456 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,23 +1,43 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
 
-        int[] stair = new int[301];
-        int[] score = new int[301];
-        for (int i = 1; i <= N; i++)
-            stair[i] = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        int n = Integer.parseInt(br.readLine());
 
-        score[1] = stair[1]; //10
-        score[2] = stair[1] + stair[2]; //30
-        score[3] = Math.max(stair[1], stair[2]) + stair[3]; //35
-
-        for (int n = 4; n <= N; n++) {
-            score[n] = Math.max(score[n - 3] + stair[n - 1], score[n - 2]) + stair[n];
+        // 계단이 1개 이하일 경우 처리
+        if (n == 1) {
+            int t = Integer.parseInt(br.readLine());
+            System.out.println(t);
+            return;
         }
 
-        System.out.println(score[N]);
+        //정의
+        int[][] dp = new int[n + 1][3];
+        int[] arr = new int[n + 1];
+
+        for(int i = 1; i <= n; i++) {
+            int t = Integer.parseInt(br.readLine());
+            arr[i] = t;
+        }
+
+        //초기값
+        dp[1][1] = arr[1];
+        dp[1][2] = Integer.MIN_VALUE;
+
+        dp[2][1] = arr[2];
+        dp[2][2] = arr[1] + arr[2];
+
+        //점화식
+        for (int i = 3; i <= n; i++) {
+            dp[i][1] = Math.max(dp[i-2][1], dp[i-2][2]) + arr[i];
+            dp[i][2] = dp[i-1][1] + arr[i];
+        }
+
+        int result = Math.max(dp[n][1], dp[n][2]);
+        System.out.println(result);
     }
 }
