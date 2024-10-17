50b24bf4a1fae5865f8f5657f560883469738ab5
KimEoJin <87289562+greeneryjin@users.noreply.github.com>
2024-10-17 14:10:21 +0900

    [Silver V] Title: 숫자 카드, Time: 996 ms, Memory: 142496 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -31,42 +31,4 @@ public class Main {
         }
         System.out.println(sb);
     }
-
-            /**
-         * 1. 먼저 해시함수에 저장을 해야함 -> 배열에 먼저 저장하고 해시함수를 찾을 경우, 값의 순서를 보장하지 못함
-         * 2. 조회가 너무 많이 일어나는 경우가 발생 -> 배열 저장, 해시 저장, 배열 조회, 해시 조회 -> 순서 맞지 않음
-         *
-         * */
-//        int[] arr = new int[n];
-//        st = new StringTokenizer(br.readLine());
-//        for(int i = 0; i < n; i++) {
-//            int data = Integer.valueOf(st.nextToken());
-//            arr[i] = data;
-//        }
-//        Arrays.sort(arr);
-//
-//        st = new StringTokenizer(br.readLine());
-//        int m = Integer.valueOf(st.nextToken());
-//        int[] ans = new int[m];
-//        HashMap<Integer, Integer> map = new HashMap<>();
-//
-//        st = new StringTokenizer(br.readLine());
-//        for(int i = 0; i < m; i++) {
-//            int data = Integer.valueOf(st.nextToken());
-//            map.put(data, 0);
-//        }
-//
-//        List<Integer> keySet = new ArrayList<>(map.keySet());
-//        Collections.sort(keySet);
-//        for(int i = 0; i < arr.length; i++) {
-//            if(keySet.contains(arr[i])) {
-//                ans[i] = 1;
-//            } else {
-//                ans[i] = 0;
-//            }
-//        }
-//
-//        for(int i = 0; i < ans.length; i++) {
-//            System.out.println(ans[i]);
-//        }
 }
