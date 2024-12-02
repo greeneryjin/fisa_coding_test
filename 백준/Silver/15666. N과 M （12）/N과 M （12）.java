1a7e999d132e201189e3f70b0252544873114d6b
KimEoJin <87289562+greeneryjin@users.noreply.github.com>
2024-12-02 12:45:01 +0900

    [Silver II] Title: N과 M (12), Time: 228 ms, Memory: 23288 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,50 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static int[] arr;
+    static boolean[] visit;
+    static int[] answer;
+    static LinkedHashSet<String> ans;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+
+        visit = new boolean[n];
+        answer = new int[n];
+        ans = new LinkedHashSet<>();
+
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st1.nextToken());
+        }
+        Arrays.sort(arr);
+        dfs(0, 0);
+        ans.forEach(System.out::println);
+    }
+
+    public static void dfs(int s, int f) {
+        if(m == f) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < m; i++) {
+                sb.append(answer[i]).append(' ');
+            }
+            ans.add(sb.toString());
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = s; i < n; i++) {
+            answer[f] = arr[i];
+            dfs(i, f + 1);
+        }
+    }
+}
